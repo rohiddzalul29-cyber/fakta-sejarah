4b7bc603d0e47f539ad70564ab0be41b55cce08e
muhammad rohid dzalul <rohiddzalul29@gmail.com>
2026-06-09 22:36:06 +0700

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,9 @@
+/**
+ * OneSignalSDKWorker.js
+ * File ini WAJIB ditempatkan di root website (domain utama).
+ * Contoh: https://websitekamu.com/OneSignalSDKWorker.js
+ *
+ * Jika menggunakan subfolder atau CDN terpisah, kamu perlu mengatur
+ * `serviceWorkerPath` di konfigurasi OneSignal.init().
+ */
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
